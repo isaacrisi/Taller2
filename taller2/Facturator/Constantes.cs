ed2973da150f718f9d1e61a5939677cf46d00cd6
isaacrisi <107268730+isaacrisi@users.noreply.github.com>
2024-05-06 06:35:42 -0500

    Update Constantes.cs

--- taller2/Facturator/Constantes.cs
+++ taller2/Facturator/Constantes.cs
@@ -1,13 +1,15 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Facturator {
-    class Constantes {
+namespace Facturator
+{
+    class Constantes
+    {
 
-        public const string NOMBRE_NEGOCIO = "PAPAS EL CALVO";
+        public const string NOMBRE_NEGOCIO = "PASADERO EL CALVO";
         public const string ERROR_AL_BUSCAR_FACTURA = "El id de la fatura no existe";
         public const int ANCHO_TIRILLA = 32;
         public const int CANTIDAD_CARACTERES_NOMBRE_PRODUCTO = 12;
