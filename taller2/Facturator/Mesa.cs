8b1fe61dee282a4d65aea07132b0778f67dfe519
isaacrisi <107268730+isaacrisi@users.noreply.github.com>
2024-05-07 15:42:19 -0500

    Update Mesa.cs

--- taller2/Facturator/Mesa.cs
+++ taller2/Facturator/Mesa.cs
@@ -1,4 +1,4 @@
-﻿
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +10,7 @@ namespace Facturator
     {
         class Mesa
         {
+        /*
             public int Numero
             {
                 get; set;
@@ -31,8 +32,45 @@ namespace Facturator
                 Productos = new List<Producto>();
 
             }
-        
+        */
+        private static HashSet<int> MesasOcupadas = new HashSet<int>();
 
+        static Mesa()
+        {
+            // Configurar la mesa tres como ocupada por defecto
+            MesasOcupadas.Add(3);
+        }
+
+        public static void MostrarEstadoMesas()
+        {
+            Console.WriteLine("Estado de las mesas:");
+            Console.WriteLine("╔══════════════════════════════╗");
+            for (int i = 1; i <= 5; i++)
+            {
+                string estado = MesasOcupadas.Contains(i) ? "Ocupada" : "Libre";
+                string mesa = $"Mesa {i}: {estado}";
+                Console.WriteLine($"║ {mesa,-28} ║");
+            }
+            Console.WriteLine("╚══════════════════════════════╝");
+        }
+
+        public static int ElegirMesa()
+        {
+            Console.WriteLine("Por favor, elija una mesa disponible:");
+            MostrarEstadoMesas();
+            int mesa;
+            while (!int.TryParse(Console.ReadLine(), out mesa) || mesa < 1 || mesa > 5 || MesasOcupadas.Contains(mesa))
+            {
+                Console.WriteLine("Mesa inválida o ocupada. Por favor, elija otra mesa.");
+            }
+            MesasOcupadas.Add(mesa); // Marcar la mesa como ocupada
+            return mesa;
+        }
+
+        public static void LiberarMesa(int mesa)
+        {
+            MesasOcupadas.Remove(mesa); // Marcar la mesa como liberada
+        }
     }
     }
 
