5895b64b79dbb01c343eb34a18f3f9fe3754051a
isaacrisi <107268730+isaacrisi@users.noreply.github.com>
2024-05-07 15:42:39 -0500

    Update VentaManager.cs

--- taller2/Facturator/VentaManager.cs
+++ taller2/Facturator/VentaManager.cs
@@ -9,16 +9,21 @@ namespace Facturator
 {
     internal class VentaManager
     {
+        Mesa mesa1 = new Mesa();
 
         public static void RealizarVenta(Caja caja)
         {
             Console.Clear();
+            int mesa = Mesa.ElegirMesa(); // El usuario elige una mesa
+            Console.WriteLine($"Mesa {mesa} seleccionada.");
             UI.MostrarProductos(caja);
             List<Producto> productosSeleccionados = EscogerProductos(caja);
-            float total = CalcularTotal(productosSeleccionados);
-            Console.WriteLine($"El total a pagar es: ${total}");
+            float total = CalcularTotal(productosSeleccionados, mesa); // Pasar el número de mesa
+            Console.WriteLine($"El total a pagar para la Mesa {mesa} es: ${total}");
             Factura factura = new Factura();
             factura.Productos1 = productosSeleccionados;
+            Mesa.LiberarMesa(mesa); // Liberar la mesa después de la venta
+
 
         }
 
@@ -62,13 +67,15 @@ namespace Facturator
             return productosSeleccionados;
         }
 
-        public static float CalcularTotal(List<Producto> productos)
+        public static float CalcularTotal(List<Producto> productos, int mesa)
         {
             float total = 0;
 
-            // Imprimir encabezado de la factura
+            // Imprimir encabezado de la factura con el número de mesa
             Factura factura = new Factura();
             factura.ImprimirCabezote();
+            Utilitario.ImprimirSeparador('_', Constantes.ANCHO_TIRILLA);
+            Console.WriteLine($"Mesa: {mesa}");
             Utilitario.ImprimirSeparador('_', Constantes.ANCHO_TIRILLA);
             Console.WriteLine("Producto     Precio     Cantidad");
             Utilitario.ImprimirSeparador('-', Constantes.ANCHO_TIRILLA);
@@ -86,13 +93,8 @@ namespace Facturator
                 total += subtotalProducto;
             }
 
-
             Utilitario.ImprimirSeparador('-', Constantes.ANCHO_TIRILLA);
-
-
             Console.WriteLine($"TOTAL: ${total}");
-
-
             Utilitario.ImprimirSeparador('*', Constantes.ANCHO_TIRILLA);
 
             return total;
@@ -100,3 +102,7 @@ namespace Facturator
 
     }
 }
+
+
+
+
